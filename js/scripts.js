a4d80c239b2f51207a1354570e2f1adfa062d6a9
Newton Bii <biinewton382@gmail.com>
2017-11-04 00:43:02 +0300

    merge master.

--- js/scripts.js
+++ js/scripts.js
@@ -40,21 +40,23 @@ var playFn = function(){
 $("#btn-start").click(function(event){
   $("#player1-tag").empty();
   $("#player2-tag").empty();
+  $("input#player1").append(" ");
+  $("input#player2").append(" ");
 
-  $("#new-game-register").fadeIn(2000);
+  $("#new-game-register").fadeIn(500);
 });
 
 $("#btn-register").click(function(event){
 event.preventDefault();
 
-  $(".game-panel").slideDown(2000);
-  $("#new-game-register").fadeOut(2000);
+  $(".game-panel").show(800);
+  $("#new-game-register").fadeOut(1000);
 
   var player1Name = $("#player1").val();
-  $("#player1-tag").append(player1Name);
+  $("#player1-tag").append("PLAYER 1: "+player1Name);
 
   var player2Name = $("#player2").val();
-  $("#player2-tag").append(player2Name);
+  $("#player2-tag").append("PLAYER 2: "+player2Name);
 
   player1.playerName=player1Name;
   player2.playerName=player2Name;
