17af0ca6e46b53b4c904a28ae36e43b7f0f4b34a
Newton Bii <biinewton382@gmail.com>
2017-11-03 15:26:49 +0300

    Add table for displaying results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,51 @@
-var number = Math.floor((Math.random()*6)+1);
+//business logic
 
-var player1Fn=function () {
+var player1 = "";
+var player2 = "";
+
+var playFn = function(){
+  return Math.floor((Math.random()*6)+1);
+}
+
+    //create constructor for properties
+ function Rounds(players){
+   this.play=0;
+   this.rolledScore=0;
+   this.totalScore=0;
+   this.players=players;
+ }
+
+ //create function for firstplay
+ Rounds.prototype.firstPlay = function(){
+   if (this.play==1) {
+     this.rolledScore=0;
+     alert("You rolled 1. Next player's turn");
+   }else {
+     this.rolledScore += this.play;
+   }
+ };
+
+ //function for holding.
+
+ Rounds.prototype.hold = function () {
+
+   this.totalScore += this.rolledScore;
+   this.rolledScore = 0;
+   alert("HOLD. Next Player's Turn");
+
+ };
+
+
+var player1Fn = function () {
   var player1=number;
+  for (player1 <=1; i++) {
+    array[i]
+  }
 
+  var
   if (player1<=1) {
     alert("Player 2's Turn")
   } else {
-    alert(player1)
+
   }
 }
