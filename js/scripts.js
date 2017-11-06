af1d0f81e8f1ad726894b31c11284d3f6bdd00c9
Newton Bii <biinewton382@gmail.com>
2017-11-06 07:16:30 +0300

    Add readme.

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,5 @@
 //business logic
-<<<<<<< HEAD
 
-var player1 = "";
-var player2 = "";
-
-var playFn = function(){
-  return Math.floor((Math.random()*6)+1);
-}
-
-    //create constructor for properties
- function Rounds(players){
-   this.play=0;
-   this.rolledScore=0;
-   this.totalScore=0;
-   this.players=players;
- }
-
- //create function for firstplay
- Rounds.prototype.firstPlay = function(){
-   if (this.play==1) {
-     this.rolledScore=0;
-     alert("You rolled 1. Next player's turn");
-   }else {
-     this.rolledScore += this.play;
-   }
- };
-
- //function for holding.
-
- Rounds.prototype.hold = function () {
-
-   this.totalScore += this.rolledScore;
-   this.rolledScore = 0;
-   alert("HOLD. Next Player's Turn");
-
- };
-
-
-//user interface
-$("#btn-start").click(function(event){
-  $("#player1-tag").empty();
-  $("#player2-tag").empty();
-  $("input#player1").append(" ");
-  $("input#player2").append(" ");
-
-  $("#new-game-register").fadeIn(500);
-});
-
-$("#btn-register").click(function(event){
-event.preventDefault();
-
-  $(".game-panel").show(800);
-  $("#new-game-register").fadeOut(1000);
-
-  var player1Name = $("#player1").val();
-  $("#player1-tag").append("PLAYER 1: "+player1Name);
-
-  var player2Name = $("#player2").val();
-  $("#player2-tag").append("PLAYER 2: "+player2Name);
-
-  player1.playerName=player1Name;
-  player2.playerName=player2Name;
-=======
 function Player(playerName, turnScore, totalScore) {
   this.playerName = playerName;
   this.turnScore = turnScore;
@@ -173,6 +111,5 @@ event.preventDefault();
 
     }
   });
->>>>>>> gh-pages
 
 });
---
+++ readme.md
@@ -0,0 +1,41 @@
+# PIG DICE
+
+#### This is a project for pig dice game that allows two users to play a game of dice.
+#### By **Newton Bii**
+
+## Description
+
+Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to "hold":
+
+* If the player rolls a 1, they score nothing and it becomes the next player's turn.
+* If the player rolls any other number, it is added to their turn total and the player's turn continues.
+* If a player chooses to "hold", their turn total is added to their score, and it becomes the next player's turn.
+* The first player to score 100 or more points wins.
+
+For example, the first player, Ann, begins a turn with a roll of 5. Ann could hold and score 5 points, but chooses to roll again. Ann rolls a 2, and could hold with a turn total of 7 points, but chooses to roll again. Ann rolls a 1, and must end her turn without scoring. The next player, Bob, rolls the sequence 4-5-3-5-5, after which he chooses to hold, and adds his turn total of 22 points to his score.
+
+## Setup/Installation Requirements
+
+* Go to https://newtonbii.github.io/pig-dice/
+
+
+## Technologies Used
+
+Technologiess used include:
+
+* HTML
+* CSS.
+* JavaScript
+* jQuery-3.2.0.js
+* Bootstrap
+
+
+## Support and contact details
+
+If you have further additions, subtractions or queries, you can got to the repository as https://github.com/NewtonBii/pig-dice and make additions.
+
+### License
+
+*MIT Licence*
+
+Copyright (c) 2017 **SMAVE Digital**
